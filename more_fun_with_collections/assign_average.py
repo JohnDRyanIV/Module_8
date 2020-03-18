93b57c093fc30c3a93fca060e642190e8fd086da
Diamondfiend <jdryan30@yahoo.com>
2020-03-18 14:34:27 -0500

    implemented assign_average.py to work with first test, modified tests to comply with existing dictionary in switch_average rather than passing it (couldn't find any documentation on how to implement a switch statement with a passed dictionary), changed value error in final test to key error.

--- more_fun_with_collections/assign_average.py
+++ more_fun_with_collections/assign_average.py
@@ -1,4 +1,15 @@
-def switch_average(dict_a, key):
-    pass
+def switch_average(key):
+    """
+    :param key: key being searched for in dictionary
+    :return: value at key's location in dictionary
+    """
+    switcher = {
+        'A': 90,
+        'B': 80,
+        'C': 70,
+        'D': 60,
+        'F':  0
+    }
+    return switcher.get(key)
 
 
--- test/test_assign_average.py
+++ test/test_assign_average.py
@@ -1,19 +1,18 @@
 import unittest
 from more_fun_with_collections import assign_average as a
-DICT_TEST = {'A': 90, 'B': 80, 'C': 70, 'D': 60, 'F': 0}
 
 
 class MyTestCase(unittest.TestCase):
 
     def test_keys(self):  # Including all keys in one test due to similarity of tests
-        self.assertEqual(a.switch_average(DICT_TEST, 'A'), 90)
-        self.assertEqual(a.switch_average(DICT_TEST, 'B'), 80)
-        self.assertEqual(a.switch_average(DICT_TEST, 'C'), 70)
-        self.assertEqual(a.switch_average(DICT_TEST, 'D'), 60)
-        self.assertEqual(a.switch_average(DICT_TEST, 'F'),  0)
+        self.assertEqual(a.switch_average('A'), 90)
+        self.assertEqual(a.switch_average('B'), 80)
+        self.assertEqual(a.switch_average('C'), 70)
+        self.assertEqual(a.switch_average('D'), 60)
+        self.assertEqual(a.switch_average('F'),  0)
 
     def test_non_key(self):
-        self.assertEqual(a.switch_average(DICT_TEST, 'Q'), ValueError)
+        self.assertEqual(a.switch_average('Q'), KeyError)
 
 
 
