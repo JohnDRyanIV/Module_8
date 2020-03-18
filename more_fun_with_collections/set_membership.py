38a601d47f855946f00dd442b68d7be43e93c7e1
Diamondfiend <jdryan30@yahoo.com>
2020-03-18 12:15:08 -0500

    all tests pass

--- more_fun_with_collections/set_membership.py
+++ more_fun_with_collections/set_membership.py
@@ -1,8 +1,9 @@
-def in_set(a, val):
+def in_set(set_a, val):
     """
     :param a: set being combed through
     :param val: value being searched for in set
     :return: boolean on whether or not set is found
     """
-    pass
-
+    if val in set_a:
+        return True
+    return False
